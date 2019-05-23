cf93fec7d5b58e783838bfb472f16def836495e3
wai81 <50743966+wai81@users.noreply.github.com>
2019-05-24 01:03:47 +0300

    edit sutup.sh

--- setup.sh
+++ setup.sh
@@ -6,14 +6,14 @@ REDASH_BASE_PATH=/opt/redash
 
 install_docker(){
     # Install Docker
-    sudo apt-get update
+    sudo apt-get update 
     sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget pwgen
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) s$
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update && sudo apt-get -y install docker-ce
 
     # Install Docker Compose
-    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(un$
+    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
     # Allow current user to run Docker commands
@@ -31,7 +31,6 @@ create_directories() {
     fi
 }
 
-
 create_config() {
     if [[ -e $REDASH_BASE_PATH/env ]]; then
         now=$(date +%s)
@@ -42,6 +41,7 @@ create_config() {
     COOKIE_SECRET=$(pwgen -1s 32)
     POSTGRES_PASSWORD=$(pwgen -1s 32)
     REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
+    
 
     echo "PYTHONUNBUFFERED=0" >> $REDASH_BASE_PATH/env
     echo "REDASH_LOG_LEVEL=INFO" >> $REDASH_BASE_PATH/env
@@ -49,10 +49,10 @@ create_config() {
     echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
     echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
     echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
+    
 }
 
 
 install_docker
 create_directories
 create_config
-
