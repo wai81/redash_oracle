bb167a251435f069054b7001e456aad320ba3a75
wai81 <vashkelevich.andrei@gmail.com>
2019-05-30 13:57:47 +0300

    Udate files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 # This script setups dockerized Redash on Ubuntu 18.04.
 set -eu
@@ -9,11 +10,11 @@ install_docker(){
     sudo apt-get update
     sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common wget pwgen
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) s$
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update && sudo apt-get -y install docker-ce
 
     # Install Docker Compose
-    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(un$
+    sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
     # Allow current user to run Docker commands
@@ -31,7 +32,6 @@ create_directories() {
     fi
 }
 
-
 create_config() {
     if [[ -e $REDASH_BASE_PATH/env ]]; then
         now=$(date +%s)
@@ -42,6 +42,7 @@ create_config() {
     COOKIE_SECRET=$(pwgen -1s 32)
     POSTGRES_PASSWORD=$(pwgen -1s 32)
     REDASH_DATABASE_URL="postgresql://postgres:${POSTGRES_PASSWORD}@postgres/postgres"
+    NLS_LANG=Russian_Russia.UTF8
 
     echo "PYTHONUNBUFFERED=0" >> $REDASH_BASE_PATH/env
     echo "REDASH_LOG_LEVEL=INFO" >> $REDASH_BASE_PATH/env
@@ -49,10 +50,10 @@ create_config() {
     echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> $REDASH_BASE_PATH/env
     echo "REDASH_COOKIE_SECRET=$COOKIE_SECRET" >> $REDASH_BASE_PATH/env
     echo "REDASH_DATABASE_URL=$REDASH_DATABASE_URL" >> $REDASH_BASE_PATH/env
+    echo "NLS_LANG=$NLS_LANG" >> $REDASH_BASE_PATH/env
 }
 
 
 install_docker
 create_directories
 create_config
-
